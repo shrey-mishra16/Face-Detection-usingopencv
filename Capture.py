e90695418bf09fb47463ae3ccc048d292217ed5e
Shrey Mishra <shreymmishra16@gmail.com>
2019-01-01 17:58:40 +0530

    Add files via upload

--- Capture.py
+++ Capture.py
@@ -3,15 +3,23 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+face_casc=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while True:
 	ret,frame=cap.read()
-	#frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+	#faces will store different faces as list of tuples with face coordinates
+	faces=face_casc.detectMultiScale(gray_frame,1.3,5)#1.3 is scaing factor
 
 	if (ret== False):
 		continue
-	cv2	.imshow("VideoFrame",frame)
+	
 
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+	cv2	.imshow("VideoFrame",frame)
 	#Wait for q to stop
 
 	key_pressed=cv2.waitKey(1)&0xFF
